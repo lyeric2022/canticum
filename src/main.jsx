47e1e927a81b6f938413881617a2fa1f32418998
Brenda Pham <blpham21@gmail.com>
2024-05-02 23:21:41 -0700

    Undo changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Home from "./HomePage/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/recommendations",
-    element: <Recommendations />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
