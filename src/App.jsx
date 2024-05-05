65fd0673be3f3269ef6626cb83a8f42200bf8f6f
Brenda Pham <blpham21@gmail.com>
2024-05-05 11:58:40 -0700

    Implement react router for home and recommendations page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div id="main">
-      <div id="title">
-        <p>Canticum</p>
-      </div>
-      <div id="input-box">
-        <input id="song-input" type="text" placeholder="search for a song" />
-        <div id="go-button">
-          <button>go!</button>
-        </div>
-      </div>
-      <div id="desc-div">
-        <h1>about</h1>
-        <text>description</text>
-        <h1>directions</h1>
-        <text>directions</text>
-      </div>
-    </div>
-  );
+  return <div id="main">Canticum</div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./HomePage/Home";
+import Recommendations from "./RecPage/Recommendations.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/recommendations",
+    element: <Recommendations />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
