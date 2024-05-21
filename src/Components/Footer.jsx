63211e03d13ac052d6e16dc69b7521f0805b2a50
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2024-05-20 20:53:24 -0700

    test deploy

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+// Footer.js
+
+import React from 'react';
+import './Footer.css'; // Import the CSS file for styling
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>made with ❤️ from eric ly and brenda pham</p>
+    </footer>
+  );
+}
+
+export default Footer;
